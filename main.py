0007a6f85146ff67c8fba4de03ec4a7abb03d51a
pyto86 <pyto86.me@gmail.com>
2020-08-18 01:38:03 +0900

    impl main.py

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import logging
 import grpc
 from grpc_reflection.v1alpha import reflection
 
-from pb.helloworld_pb2 import DESCRIPTOR
+from pb.helloworld_pb2 import DESCRIPTOR as HELLOWORLD_DESCRIPTOR
 from pb.helloworld_pb2_grpc import add_GreeterServicer_to_server
+from pb.user_pb2 import DESCRIPTOR as USER_DESCRIPTOR
+from pb.user_pb2_grpc import add_UserServiceServicer_to_server
 
 from app.greeter import GreeterServicer
+from app.user import UserServiceServicer
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     add_GreeterServicer_to_server(
         GreeterServicer(), server)
+    add_UserServiceServicer_to_server(
+        UserServiceServicer(), server)
     SERVICE_NAMES = (
-        DESCRIPTOR.services_by_name['Greeter'].full_name,
+        HELLOWORLD_DESCRIPTOR.services_by_name['Greeter'].full_name,
+        USER_DESCRIPTOR.services_by_name['UserService'].full_name,
         reflection.SERVICE_NAME,
     )
     reflection.enable_server_reflection(SERVICE_NAMES, server)
