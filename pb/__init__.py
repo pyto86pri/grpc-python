68d82dfa61a4d8e4805ec3422d53014cfa5358f6
pyto86 <pyto86.me@gmail.com>
2020-08-17 19:01:22 +0900

    add reference to the solution

--- pb/__init__.py
+++ pb/__init__.py
@@ -1,6 +1,7 @@
 """
 Need to add this because `xxx_pb2_grpc.py` fail to import `xxx_pb2.py`
 unless `xxx_pb2_grpc.py` and `xxx_pb2.py` are in the root directory.
+ref. https://github.com/protocolbuffers/protobuf/issues/1491#issuecomment-547504972
 """
 import sys
 from pathlib import Path
