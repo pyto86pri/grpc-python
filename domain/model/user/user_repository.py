07c6441e7fa70cdd1af64051b85daf52b5f91828
pyto86 <pyto86.me@gmail.com>
2020-08-18 12:30:03 +0900

    not to use abc

--- domain/model/user/user_repository.py
+++ domain/model/user/user_repository.py
@@ -1,34 +1,28 @@
 """
 ユーザーリポジトリ．
 """
-from abc import ABCMeta, abstractmethod
 from typing import List
 
 from .user import UserId, User
 
 
-class IUserRepository(metaclass=ABCMeta):
-    @abstractmethod
+class IUserRepository:
     def nextId(self) -> UserId:
         """ Get next user id """
-        pass
+        raise NotImplementedError("")
 
-    @abstractmethod
     def get(self, id_: UserId) -> User:
         """ Get user by id """
-        pass
+        raise NotImplementedError("")
 
-    @abstractmethod
     def save(self, user: User) -> None:
         """ Save user """
-        pass
+        raise NotImplementedError("")
 
-    @abstractmethod
-    def delete(self, id_: UserId) -> User:
+    def delete(self, id_: UserId) -> None:
         """ Delete user """
-        pass
+        raise NotImplementedError("")
 
-    @abstractmethod
     def list_(self) -> List[User]:
         """ List all users """
-        pass
+        raise NotImplementedError("")
