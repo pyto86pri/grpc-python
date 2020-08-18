11a30a1a1ddd3f97fcfbb72980ea5bf6929d01e4
pyto86 <pyto86.me@gmail.com>
2020-08-18 14:58:27 +0900

    fix mysql init script

--- script/mysql/init.sh
+++ script/mysql/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mysql -u user -ppassword test <<-EOSQL
-    DROP TABLE IF EXISTS `users`;
+    DROP TABLE IF EXISTS users;
 
-    CREATE TABLE IF NOT EXISTS `users`
+    CREATE TABLE IF NOT EXISTS users
     (
-        `id`     VARCHAR(20) NOT NULL,
-        `name`   VARCHAR(20) NOT NULL,
-            PRIMARY KEY (`id`)
+        id     VARCHAR(200) NOT NULL,
+        name   VARCHAR(200) NOT NULL,
+            PRIMARY KEY (id)
     )
 EOSQL
